873bccbca138357d33dd120f28ec1bb201b0ddcd
JohniCassere <johnicassere@hotmail.com>
2022-02-23 21:45:09 -0300

    Add Swagger

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,28 @@
 import { NestFactory } from '@nestjs/core';
+import { ValidationPipe } from '@nestjs/common';
 import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+
+
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3001);
+
+  app.enableCors();
+
+  app.useGlobalPipes(new ValidationPipe());
+
+  const config = new DocumentBuilder()
+    .setTitle('Bingo')
+    .setDescription('Bingo Pipa Studios')
+    .setVersion('1.0')
+    .addTag('Bingo')
+    .addBearerAuth()
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
+  await app.listen(process.env.PORT || 3001);
 }
 bootstrap();
